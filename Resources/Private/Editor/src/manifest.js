5768624834cd102a9e02d565afba3a56d6cd3ae9
Jon Uhlmann <account@uhlmann.pro>
2021-12-10 21:23:19 +0100

    Chore: Code style

--- Resources/Private/Editor/src/manifest.js
+++ Resources/Private/Editor/src/manifest.js
@@ -1,8 +1,14 @@
-import manifest from "@neos-project/neos-ui-extensibility";
+import manifest from '@neos-project/neos-ui-extensibility';
 
-import ColorValuesEditor from "./ColorValuesEditor";
+import ColorValuesEditor from './ColorValuesEditor';
 
-manifest("Carbon.ColorValues:Editor", {}, (globalRegistry, { frontendConfiguration }) => {
-    const editorsRegistry = globalRegistry.get("inspector").get("editors");
-    editorsRegistry.set("Carbon.ColorValues/Editor", { component: ColorValuesEditor });
-});
+manifest(
+  'Carbon.ColorValues:Editor',
+  {},
+  (globalRegistry, { frontendConfiguration }) => {
+    const editorsRegistry = globalRegistry.get('inspector').get('editors');
+    editorsRegistry.set('Carbon.ColorValues/Editor', {
+      component: ColorValuesEditor,
+    });
+  }
+);
